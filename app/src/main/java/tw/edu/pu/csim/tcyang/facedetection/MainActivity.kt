07ba014d960167a17bd23f9c4e1c1b1c35ed24d9
tcyang <tcyang@gm.pu.edu.tw>
2021-04-24 19:08:02 +0800

    Configure the face detector

--- app/src/main/java/tw/edu/pu/csim/tcyang/facedetection/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/facedetection/MainActivity.kt
@@ -4,6 +4,9 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetectorOptions
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,5 +20,16 @@ class MainActivity : AppCompatActivity() {
         //將drawable的圖片轉換成Bitmap
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.family)
         img.setImageBitmap(bitmap)
+
+        //Configure the face detector
+        val options = FaceDetectorOptions.Builder()
+            .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
+            .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_ALL)
+            .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_ALL)
+            .build()
+
+        //Prepare the input image
+        val image = InputImage.fromBitmap(bitmap, 0)
+        val detector = FaceDetection.getClient(options)
     }
 }
