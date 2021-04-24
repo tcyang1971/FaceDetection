07bdccc307f69a828eed8d8f19134070a655e5b7
tcyang <tcyang@gm.pu.edu.tw>
2021-04-24 18:52:09 +0800

    顯示Bitmap點陣圖

--- app/src/main/java/tw/edu/pu/csim/tcyang/facedetection/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/facedetection/MainActivity.kt
@@ -1,11 +1,21 @@
 package tw.edu.pu.csim.tcyang.facedetection
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bitmap: Bitmap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //將drawable的圖片轉換成Bitmap
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.family)
+        img.setImageBitmap(bitmap)
     }
 }
